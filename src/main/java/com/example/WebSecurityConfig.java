a25e073b03eb1c88a5cccf6e963efe8b4c7dbfa1
Laborawaty Rajagukguk <laborawaty@bizzy.co.id>
2018-04-24 21:04:34 +0700

    mengambil username dan password dari database

--- src/main/java/com/example/WebSecurityConfig.java
+++ src/main/java/com/example/WebSecurityConfig.java
@@ -1,6 +1,9 @@
 package com.example;
 
+import javax.activation.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
+@ComponentScan(value="org.webapp")
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception
@@ -27,13 +31,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	 .logout()
 	 .permitAll();
 	}
+//	@Autowired
+//	public void configureGlobal (AuthenticationManagerBuilder auth) throws Exception
+//	{
+//	 auth.inMemoryAuthentication()
+//	 .withUser("admin").password("admin")
+//	 .roles("ADMIN")
+//	 .and().withUser("user").password("user")
+//     .roles("USER");
+//	}
+	
 	@Autowired
-	public void configureGlobal (AuthenticationManagerBuilder auth) throws Exception
+	javax.sql.DataSource dataSource;
+	
+	@Autowired
+	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception
 	{
-	 auth.inMemoryAuthentication()
-	 .withUser("admin").password("admin")
-	 .roles("ADMIN")
-	 .and().withUser("user").password("user")
-     .roles("USER");
+	auth.jdbcAuthentication().dataSource(dataSource)
+		.usersByUsernameQuery("select username,password,enabled from users where username=?")
+		.authoritiesByUsernameQuery("select username, role from user_roles where username=?");
 	}
 }
